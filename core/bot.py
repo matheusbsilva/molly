a3f5ece449613be071e1cca08138569b7f05523b
matheusbsilva <matheusbattista@hotmail.com>
2019-08-06 12:01:06 -0300

    Setup execution type for webhook and polling

--- core/bot.py
+++ core/bot.py
@@ -9,10 +9,29 @@ def hello(bot, update):
     update.message.reply_text(
             'Hello {}'.format(update.message.from_user.first_name))
 
-def run(token):
+
+def start_polling(updater):
+    updater.start_polling()
+    updater.idle()
+
+
+def start_webhook(updater, host='0.0.0.0', port=80, webhook_url=None):
+    if webhook_url == None:
+        raise ValueError('Webhook URL can not be None')
+
+    updater.start_webhook(listen=host, port=port)
+    updater.bot.set_webhook(webhook_url)
+    updater.idle()
+
+
+def run(token, execution_type='polling'):
     setup_log()
 
     updater = Updater(token)
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
-    updater.start_polling()
-    updater.idle()
+
+    if execution_type == 'webhook':
+        start_webhook(updater)
+    elif execution_type == 'polling':
+        start_polling(updater)
+
